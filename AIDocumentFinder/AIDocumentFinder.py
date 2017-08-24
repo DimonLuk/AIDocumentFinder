a9dc7e9e18ac493d39a6960b6d17b2624f5518ee
DimonLuk <lds4ever2000@gmail.com>
2017-08-25 00:16:18 +0300

    Need Review a lot

--- AIDocumentFinder/AIDocumentFinder.py
+++ AIDocumentFinder/AIDocumentFinder.py
@@ -19,7 +19,13 @@ def getTextFromWordDocument(path, fileName, *, debugging=False):
     import win32com.client
     app = win32com.client.Dispatch("Word.Application")
     pathToFile = r"%s\%s" % (path, fileName)
-    return app.Documents.Open(pathToFile).Content.Text
+    try:
+        return app.Documents.Open(pathToFile).Content.Text
+    except win32com.client.pywintypes.com_error:
+        return ""
+    except AttributeError:
+        return ""
+
 
 
 
@@ -70,7 +76,7 @@ def createArrayOfWords(text, *, debuggin=False):
     text = re.findall("\w+", text)
     return text
 
-def countEveryWord(words):
+def countEveryWord(words,*, filter=[], wordLengthMoreThan=4):
     """
     Using: counts how many times each word appears in the array of words
     than delete words witihout any information like "the", "and" and so on
@@ -78,47 +84,64 @@ def countEveryWord(words):
     from collections import Counter
     c = Counter(words)
     #Deletes trash words
-    del c["та"]
-    del c["на"]
-    del c["таеп"]
-    del c["з"]
-    del c["в"]
-    del c["і"]
-    del c["що"]
-    del c["зв"]
-    del c["їх"]
-    del c["ня"]
-    del c["для"]
-    del c["gc"]
-    del c["of"]
-    del c["the"]
-    del c["при"]
-    del c["за"]
-    del c["end"]
-    del c["and"]
-    for i in range(1000):
-        tmp = "%s" % i
-        del c[tmp]
-    alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯАБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    lalphabet = alphabet.lower()
-    for i in alphabet:
-        del c[i]
-    for i in lalphabet:
-        del c[i]
+    if (not wordLengthMoreThan) and (not filter):
+        del c["та"]
+        del c["на"]
+        del c["таеп"]
+        del c["з"]
+        del c["в"]
+        del c["і"]
+        del c["що"]
+        del c["зв"]
+        del c["їх"]
+        del c["ня"]
+        del c["для"]
+        del c["gc"]
+        del c["of"]
+        del c["the"]
+        del c["при"]
+        del c["за"]
+        del c["end"]
+        del c["and"]
+        for i in range(1000):
+            tmp = "%s" % i
+            del c[tmp]
+        alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯАБВГДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        lalphabet = alphabet.lower()
+        for i in alphabet:
+            del c[i]
+        for i in lalphabet:
+            del c[i]
+    elif filter:
+        for item in c:
+            for i in filter:
+                if item != i:
+                    del c[item]
+    elif wordLengthMoreThan:
+        toDelete = []
+        for item in c:
+            if len(item) <= wordLengthMoreThan:
+                toDelete.append(item)
+        for item in toDelete:
+            del c[item]
+    
     #Return the result
-    return c
+    return (c,sum(c.values()))
 
 
 
-def getResultOfCounting(countedWords, accuracy, fileName, file):
+def getResultOfCounting(infoAboutWords, accuracy, fileName, file):
     """
     Using with write=False - returns info about most common words which number is set with accuracy
     Using with write=True - writes to file info about most common words which number is set with accuracy
     """
-    text = "---------------------------------------------\n%s\n" % fileName
+    text = "---------------------------------------------\n%s\n%s\n" % (fileName, infoAboutWords[1])
     file.write(text)
-    for word in countedWords.most_common(accuracy):
-        file.write("%s:%s\n" % word)
+    for word in infoAboutWords[0].most_common(accuracy):
+        try:
+            file.write("%s:%s\n" % (word[0], word[1]))
+        except UnicodeEncodeError:
+            pass
 
 
 def downloadDocuments(pathToSave, extension="doc", *, fromInternet=False, link="", pathToFiles=""):
@@ -165,6 +188,8 @@ def downloadDocuments(pathToSave, extension="doc", *, fromInternet=False, link="
                     #Create normal name
                     for part in parts:
                         if (".%s" % extension) in part:
+                            if len(part) > 20:
+                                fileName = part[len(part)-10:]+"."+extension
                             fileName = part
                     fullPath = r"%s\%s" % (pathToSave, fileName)
                     try:
@@ -176,24 +201,74 @@ def downloadDocuments(pathToSave, extension="doc", *, fromInternet=False, link="
                             fileWithSources.write("\n")
                     except FileNotFoundError:
                         pass
-            fileWithSources.write("-----------------------------------------\n")
+                    except OSError:
+                        pass
     fileWithSources.close()
 
 def getPagesFromFiles(pathToFiles):
     import os
     pages = []
-    try:
-        for dir, subdirs, files in os.walk(pathToFiles):
-            for file in files:
-                absPath = "%s\%s" % (pathToFiles, file)
-                with open(absPath, "rb") as doc:
-                    pages.append(doc.read().decode("utf-8"))
-    except StopIteration:
-        pass
+    i = 0
+    for dir, subdirs, files in os.walk(pathToFiles):
+        for file in files:
+            if i == len(files):
+                break
+            absPath = "%s\%s" % (pathToFiles, file)
+            with open(absPath, "rb") as doc:
+                pages.append(doc.read().decode("utf-8"))
+            print("Proceed: ",(i/len(files))*100, "%")
+            i += 1
     return pages
 def getPagesFromTheInternet(link):
     pass
+def countWordsInFiles(pathToFiles,*, extension="doc"):
+    import os
+    info = open("%s\info.txt" % pathToFiles, "w")
+    i = 0
+    for dir, subdirs, files in os.walk(pathToFiles):
+        numOfFiles = len(files)
+        if debug:
+            print(numOfFiles)
+        for file in files:
+            if ((i/numOfFiles)*100) == 100:
+                break
+            if extension == "doc":
+                if (".doc" in file):
+                    getResultOfCounting(countEveryWord(createArrayOfWords(getTextFromWordDocument(pathToFiles, file)), wordLengthMoreThan=5), 20, file, info)
+            if debug:
+                text = "Proceed: %s" % ((i/numOfFiles)*100)
+                text += "%"
+                print(text)
+                print(i)
+                print(file)
+            i += 1
+    info.close()
+#----------------------Analysing--------------------------
+def countTheMostPopularWords(path, accuracy):
+    import re
+    import collections
+    pattern1 = re.compile(r"\w+:\d+")
+    pattern2 = re.compile("\w+:")
+    c = collections.Counter()
+    with open("%s\info.txt" % path, "r") as file:
+        while file.readline():
+            text = file.readline()[:-1]
+            if pattern1.findall(text):
+                text = pattern2.findall(text)[0][:-1]
+                c[text] += 1
+    
+    with open("%s\computedInfo.txt" % path, "w") as file:
+        for item in c.most_common(accuracy):
+            file.write("-------------------------------------------\n")
+            file.write("%s:%s\n" % (item[0], item[1]))
+
+def plotNumsOfWords(path, accuracy):
+    pass
+
     
 if __name__ == "__main__":
     if debug:
-        downloadDocuments("D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\docs", pathToFiles="D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\htmls")
+        """downloadDocuments("D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\docs", pathToFiles="D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\htmls")
+        countWordsInFiles("D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\docs")"""
+        #countTheMostPopularWords("D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\docs", 20)
+        #plotNumsOfWords("D:\Projects\AIIDocumentFinder\Alpha\AIDocumentFinder\docs", 20)
